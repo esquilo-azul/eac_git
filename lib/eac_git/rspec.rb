0d8d641cf5b84f2367f5ffb39b3cacec775f642b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-03-26 09:47:56 -0300

    EacGit::Rspec::StubbedGitLocalRepo.

--- lib/eac_git/rspec.rb
+++ lib/eac_git/rspec.rb
@@ -5,12 +5,17 @@ require 'eac_git/executables'
 
 module EacGit
   module Rspec
+    require_sub __FILE__
+
     class << self
       def configure
         ::EacRubyUtils::Rspec::Conditional.default.add(:git) do
           ::EacGit::Executables.git.validate
         end
-        RSpec.configure { |config| ::EacRubyUtils::Rspec::Conditional.default.configure(config) }
+        RSpec.configure do |config|
+          ::EacRubyUtils::Rspec::Conditional.default.configure(config)
+          config.include ::EacGit::Rspec::StubbedGitLocalRepo
+        end
       end
     end
   end
