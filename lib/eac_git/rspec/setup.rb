45a71bf59746a9ca47014619ba7768c788bd5880
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-08-13 21:02:10 -0300

    EacGit::Rspec::Setup: adapt to "eac_ruby_utils" '0.73.0'.

--- lib/eac_git/rspec/setup.rb
+++ lib/eac_git/rspec/setup.rb
@@ -5,20 +5,18 @@ require 'eac_git/rspec/stubbed_git_local_repo'
 
 module EacGit
   module Rspec
-    class Setup
-      common_constructor :setup_obj
-
-      def perform
-        setup_conditional_git
-        setup_stubbed_git_local_repo
+    module Setup
+      def self.perform(setup_obj)
+        setup_obj.setup_conditional_git
+        setup_obj.setup_stubbed_git_local_repo
       end
 
       def setup_conditional_git
-        setup_obj.conditional(:git) { ::EacGit::Executables.git.validate }
+        conditional(:git) { ::EacGit::Executables.git.validate }
       end
 
       def setup_stubbed_git_local_repo
-        setup_obj.rspec_config.include ::EacGit::Rspec::StubbedGitLocalRepo
+        rspec_config.include ::EacGit::Rspec::StubbedGitLocalRepo
       end
     end
   end
